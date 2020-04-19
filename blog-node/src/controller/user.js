2dd1ff449c45f5a6ed84c4a78eca6aca724d6f90
vortesnail <1091331061@qq.com>
2020-04-19 23:17:35 +0800

    :pig2: crypto md5 password

--- blog-node/src/controller/user.js
+++ blog-node/src/controller/user.js
@@ -1,7 +1,11 @@
 const { exec, escape } = require('../db/mysql');
+const { genPassword } = require('../utils/cryp');
 
 const login = (username, password) => {
   username = escape(username);
+
+  // 生成加密密码
+  password = genPassword(password);
   password = escape(password);
 
   const sql = `
