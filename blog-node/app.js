93a4574dd431716b5987b6ef1cd0d11dda11584c
vortesnail <1091331061@qq.com>
2020-04-19 18:11:49 +0800

    :beetle: add access log function

--- blog-node/app.js
+++ blog-node/app.js
@@ -1,5 +1,6 @@
 const querystring = require('querystring');
 const { get, set } = require('./src/db/redis');
+const { access } = require('./src/utils/log');
 const handleBlogRouter = require('./src/route/blog');
 const handleUserRouter = require('./src/route/user');
 
@@ -38,6 +39,9 @@ const getPostData = (req) => {
 };
 
 const serverHandle = (req, res) => {
+  // 记录 access log
+  access(` ${req.method} -- ${req.url} -- ${req.headers['user-agent']} -- ${Date.now()}`);
+
   // 设置返回格式 JSON
   res.setHeader('Content-Type', 'application/json');
 
