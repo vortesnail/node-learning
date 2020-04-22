0f9a1a26f7844ccf33848d45b3d0c22190f6019d
vortesnail <1091331061@qq.com>
2020-04-23 00:07:15 +0800

    :skull: add log feature

--- blog-express/app.js
+++ blog-express/app.js
@@ -1,6 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const session = require('express-session');
@@ -17,7 +18,21 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+const ENV = process.env.NODE_ENV;
+if (ENV !== 'production') {
+  // 开发环境 | 测试环境
+  app.use(logger('dev'));
+} else {
+  // 线上环境
+  const logFileName = path.join(__dirname, 'logs', 'access.log');
+  const writeStream = fs.createWriteStream(logFileName, {
+    flags: 'a'
+  });
+  app.use(logger('combined', {
+    stream: writeStream
+  }));
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
--- blog-express/routes/blog.js
+++ blog-express/routes/blog.js
@@ -2,8 +2,9 @@ var express = require('express');
 var router = express.Router();
 const { getList, getDetail, newBlog, updateBlog, delBlog } = require('../controller/blog');
 const { SuccessModel, ErrorModel } = require('../model/resModel');
+const loginCheck = require('../middleware/loginCheck');
 
-router.get('/list', function(req, res, next) {
+router.get('/list', (req, res, next) => {
   let author = req.query.author || '';
   const keyword = req.query.keyword || '';
 
@@ -27,10 +28,53 @@ router.get('/list', function(req, res, next) {
   });
 });
 
-router.get('/detail', function(req, res, next) {
-  res.json({
-    errno: 0,
-    data: 'OK'
+router.get('/detail', (req, res, next) => {
+  const result = getDetail(req.query.id);
+  return result.then(data => {
+    res.json(
+      new SuccessModel(data)
+    );
+  });
+});
+
+router.post('/new', loginCheck, (req, res, next) => {
+  req.body.author = req.session.username;
+  const result = newBlog(req.body);
+  return result.then(data => {
+    res.json(
+      new SuccessModel(data)
+    );
+  });
+});
+
+router.post('/update', loginCheck, (req, res, next) => {
+  const result = updateBlog(req.query.id, req.body);
+  return result.then(val => {
+    if (val) {
+      res.json(
+        new SuccessModel(val)
+      );
+    } else {
+      res.json(
+        new ErrorModel('更新博客失败!')
+      );
+    }
+  });
+});
+
+router.post('/del', loginCheck, (req, res, next) => {
+  const author = req.session.username;
+  const result = delBlog(req.query.id, author);
+  return result.then(val => {
+    if (val) {
+      res.json(
+        new SuccessModel(val)
+      );
+    } else {
+      res.json(
+        new ErrorModel('删除博客失败!')
+      );
+    }
   });
 });
 
